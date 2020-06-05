b95cb5bd41349f96b0acfdee21fb39a1b03088ff
Rudo <u17296928@tuks.co.za>
2020-06-05 23:17:40 +0200

    Basic Srping Server Setup

---
+++ Server/aegis/src/test/java/com/aegis/aegis/AegisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aegis.aegis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
