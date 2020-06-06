980fd0b56ae0fb0df98274986337cf4c5c3d16cc
Pavlo-Andrianatos <andpavlo@gmail.com>
2020-06-06 22:41:12 +0200

    Setup Api for Spring server

--- Server/aegis/src/main/java/com/aegis/aegis/AegisApplication.java
+++ Server/aegis/src/main/java/com/aegis/aegis/AegisApplication.java
@@ -1,13 +1,29 @@
 package com.aegis.aegis;
 
+import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AegisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AegisApplication.class, args);
 	}
 
+	@RequestMapping(value = "/api/", method = RequestMethod.GET)
+	@ResponseBody
+	public String testCall() {
+		try{
+			return new JSONObject().put("testKey", "testVal").toString();
+		}catch(Exception e){
+			//return testCall();
+			return "{\"Error\":\"Internal Server Error\",\"Have you tried turning it\":\"off and back on again\"}";
+		}
+	}
 }
