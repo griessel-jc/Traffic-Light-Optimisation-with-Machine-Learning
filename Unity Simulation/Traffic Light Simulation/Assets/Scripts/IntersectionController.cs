bb29bca982c90f2a3a975f0759619f294ec5a2c9
AaronFacoline <u13283881@tuks.co.za>
2020-06-14 00:39:02 +0200

    added controllers for intersections

---
+++ Unity Simulation/Traffic Light Simulation/Assets/Scripts/IntersectionController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntersectionController : MonoBehaviour
+{	
+	public GameObject North;
+	public GameObject South;
+	public GameObject East;
+	public GameObject West;
+	public GameObject WestEastSplit;
+	public GameObject NorthSouthSplit;
+	
+	public GameObject Lnorth;
+	public GameObject Lsouth;
+	public GameObject Least;
+	public GameObject Lwest;
+	
+	
+	
+	// Start is called before the first frame update
+	void Start(){
+		StartCoroutine(waiter());
+	}
+	
+	IEnumerator waiter(){
+		while(true){
+			North.SetActive(false);
+			South.SetActive(false);
+			East.SetActive(true);
+			West.SetActive(true);
+			WestEastSplit.SetActive(false);
+			NorthSouthSplit.SetActive(true);
+			yield return new WaitForSeconds(10);
+			
+			North.SetActive(true);
+			South.SetActive(true);
+			East.SetActive(true);
+			West.SetActive(true);
+			WestEastSplit.SetActive(false);
+			NorthSouthSplit.SetActive(false);
+			yield return new WaitForSeconds(3);
+			
+			North.SetActive(true);
+			South.SetActive(true);
+			East.SetActive(false);
+			West.SetActive(false);
+			WestEastSplit.SetActive(true);
+			NorthSouthSplit.SetActive(false);
+			yield return new WaitForSeconds(10);
+			
+			North.SetActive(true);
+			South.SetActive(true);
+			East.SetActive(true);
+			West.SetActive(true);
+			WestEastSplit.SetActive(false);
+			NorthSouthSplit.SetActive(false);
+			yield return new WaitForSeconds(3);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update(){
+        
+	}
+}
---
+++ Unity Simulation/Traffic Light Simulation/Assets/Scripts/TsectionController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TsectionController : MonoBehaviour
+{	
+	public GameObject Entrance;
+	public GameObject Left;
+	public GameObject Right;
+	public GameObject Split;
+	
+	public GameObject Lnorth;
+	public GameObject Lsouth;
+	public GameObject Least;
+	public GameObject Lwest;
+	
+	
+	
+	// Start is called before the first frame update
+	void Start(){
+		StartCoroutine(waiter());
+	}
+	
+	IEnumerator waiter(){
+		while(true){
+			Entrance.SetActive(false);
+			Left.SetActive(true);
+			Right.SetActive(true);
+			Split.SetActive(false);
+			yield return new WaitForSeconds(10);
+			
+			Entrance.SetActive(true);
+			Left.SetActive(true);
+			Right.SetActive(true);
+			Split.SetActive(false);
+			yield return new WaitForSeconds(3);
+			
+			Entrance.SetActive(true);
+			Left.SetActive(false);
+			Right.SetActive(false);
+			Split.SetActive(true);
+			yield return new WaitForSeconds(10);
+			
+			Entrance.SetActive(true);
+			Left.SetActive(true);
+			Right.SetActive(true);
+			Split.SetActive(false);
+			yield return new WaitForSeconds(3);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update(){
+        
+	}
+}
