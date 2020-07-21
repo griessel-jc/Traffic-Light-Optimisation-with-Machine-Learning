8795a0e8ca84a8c5d4089e3cff83b3940babcf84
AaronFacoline <u13283881@tuks.co.za>
2020-07-21 15:15:53 +0200

    new pathing system initial commit

---
+++ Unity Simulation/Pathing2.0/Assets/Scripts/CurvePlot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CurvePlot : MonoBehaviour {
+    [SerializeField]
+    private Transform[] points;
+    private Vector3 gizmosPosition;
+
+    private void OnDrawGizmos() {
+        for(float i = 0; i <= 1; i += 0.05f){
+        	gizmosPosition = (Mathf.Pow(1-i, 3) * points[0].position) + 
+	    		(3 * Mathf.Pow(1-i, 2) * i * points[1].position) + 
+				(3 * (1 - i) * Mathf.Pow(i, 2) * points[2].position) + 
+				(Mathf.Pow(i, 3) * points[3].position);
+
+				Gizmos.DrawSphere(gizmosPosition, 0.1f);
+
+        }
+
+        Gizmos.DrawLine(new Vector3(points[0].position.x, points[0].position.y, points[0].position.z),
+        	new Vector3(points[1].position.x, points[1].position.y, points[1].position.z));
+
+        Gizmos.DrawLine(new Vector3(points[2].position.x, points[2].position.y, points[2].position.z),
+        	new Vector3(points[3].position.x, points[3].position.y, points[3].position.z));
+
+    }
+}
---
+++ Unity Simulation/Pathing2.0/Assets/Scripts/Path.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour {	
+	[SerializeField]
+	private Transform[] curves;
+	private int nextCurve;
+	private float speed;
+	private Vector3 carPosition;
+	private float t;
+	private bool coroutingAllowed;
+
+    void Start(){
+        nextCurve = 0;
+        t = 0f;
+        speed = 0.4f;
+        coroutingAllowed = true;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(coroutingAllowed)
+        	StartCoroutine(MoveCar(nextCurve));
+        
+    }
+
+    private IEnumerator MoveCar(int curveNum){
+    	coroutingAllowed = false;
+    	Vector3 p0 = curves[curveNum].GetChild(0).position;
+    	Vector3 p1 = curves[curveNum].GetChild(1).position;
+    	Vector3 p2 = curves[curveNum].GetChild(2).position;
+    	Vector3 p3 = curves[curveNum].GetChild(3).position;
+
+    	while(t < 1){
+    		t = t + (Time.deltaTime * speed);
+
+    		carPosition = (Mathf.Pow(1 - t, 3) * p0) +
+    			(3 * Mathf.Pow(1 - t, 2) * t * p1) +
+    			(3 * (1-t) * Mathf.Pow(t, 2) * p2) +
+    			(Mathf.Pow(t, 3) * p3);
+
+    		transform.position = carPosition;
+    		yield return new WaitForEndOfFrame();
+    	}
+
+    	t = 0f;
+
+    	nextCurve = nextCurve + 1;
+
+    	if(nextCurve > curves.Length - 1){
+    		nextCurve = 0;
+    	}
+
+    	coroutingAllowed = true;
+    }
+}
