f40ef6df683a05a3849bc8ce04a475a4f036f90e
Dylan-Krajnc <u17029318@tuks.co.za>
2020-08-18 15:41:55 +0200

    Moved basic integration/unit test functionality to this branch

---
+++ Unity Simulation/Pathing2.0/Assets/Tests/Unit Tests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.Networking;
+
+namespace Tests
+{
+    public class UnitTests
+    {
+        [Test]
+        public void UnitTestsSimplePasses()
+        {
+            //Simply to test whether the Unit Test script is working
+        }
+
+        [UnityTest]
+        public IEnumerator UnitTestCreateCarPrefab()
+        {
+            
+        }
+    }
+}
