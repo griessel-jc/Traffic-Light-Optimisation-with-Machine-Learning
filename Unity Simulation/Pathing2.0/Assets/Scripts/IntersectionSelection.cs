175df3adebac9fda4e0c6532273e4823adb2e6e5
AaronFacoline <55712353+AaronFacoline@users.noreply.github.com>
2020-08-19 14:06:53 +0200

    Enchancement unity scene (#87)
    * added 2 hill 3d model fbx
    
    * added new props fbx and updated scene with props
    
    * add new street light fbx and prefab + updated scene with props
    
    * Improved game object optimisation on trees
    
    * added more materials + updated scene with props
    
    * added intersection name UI on intersection hover + added text mesh pro package
    
    Co-authored-by: LiRen Ivan Zhang <u17013072@tuks.co.za>

---
+++ Unity Simulation/Pathing2.0/Assets/Scripts/IntersectionSelection.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class IntersectionSelection : MonoBehaviour
+{
+    public TextMeshProUGUI UI_intersection;
+
+    /*private void OnMouseDown() {
+        UI_intersection.text = this.name;
+    }*/
+
+    private void OnMouseEnter() {
+        UI_intersection.text = this.name;
+    }
+
+    private /// <summary>
+    /// Called when the mouse is not any longer over the GUIElement or Collider.
+    /// </summary>
+    void OnMouseExit()
+    {
+        UI_intersection.text = "";
+    }
+}
