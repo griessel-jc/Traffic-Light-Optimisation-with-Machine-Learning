e33cc3411dab76477649c382e349d0e6b61c37a6
Pavlo-Andrianatos <andpavlo@gmail.com>
2020-09-13 00:05:30 +0200

    Added day/night cycle and it adjusts spawning rates

---
+++ Unity Simulation/Pathing2.0/Assets/Scripts/LightingManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class LightingManager : MonoBehaviour
+{
+    //Scene References
+    [SerializeField]
+    public Light DirectionalLight;
+    [SerializeField]
+    public LightingPreset Preset;
+    //Variables
+    [SerializeField, Range(0, 24)]
+    private float timeOfDay;
+
+    [SerializeField]
+    public GameObject carSpawner;
+
+    private spawning spawningScript;
+
+    private void Start()
+    {
+        spawningScript = carSpawner.GetComponent<spawning>();
+    }
+
+    private void Update()
+    {
+        if (Preset == null)
+            return;
+
+        if (Application.isPlaying)
+        {
+            //(Replace with a reference to the game time)
+            timeOfDay += (Time.deltaTime * 0.05f);
+            timeOfDay %= 24; //Modulus to ensure always between 0-24
+            if (timeOfDay > 6 && timeOfDay < 9)
+            {
+                spawningScript.speed = 90;
+            }
+            else if(timeOfDay > 15 && timeOfDay < 18)
+            {
+                spawningScript.speed = 90;
+            }
+            else
+            {
+                spawningScript.speed = 60;
+            }
+            UpdateLighting(timeOfDay / 24f);
+        }
+        else
+        {
+            spawningScript.speed = 60;
+            UpdateLighting(timeOfDay / 24f);
+        }
+    }
+
+
+    private void UpdateLighting(float timePercent)
+    {
+        //Set ambient and fog
+        RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
+        RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
+
+        //If the directional light is set then rotate and set it's color, I actually rarely use the rotation because it casts tall shadows unless you clamp the value
+        if (DirectionalLight != null)
+        {
+            DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
+
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
+        }
+
+    }
+
+    //Try to find a directional light to use if we haven't set one
+    private void OnValidate()
+    {
+        if (DirectionalLight != null)
+            return;
+
+        //Search for lighting tab sun
+        if (RenderSettings.sun != null)
+        {
+            DirectionalLight = RenderSettings.sun;
+        }
+        //Search scene for light that fits criteria (directional)
+        else
+        {
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
+            foreach (Light light in lights)
+            {
+                if (light.type == LightType.Directional)
+                {
+                    DirectionalLight = light;
+                    return;
+                }
+            }
+        }
+    }
+}
